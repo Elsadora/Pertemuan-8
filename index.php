c2061733347f4f2b8278d1b2841f1ef7e3ede49a
Elsadora <89636417+Elsadora@users.noreply.github.com>
2022-04-29 19:31:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -101,13 +101,13 @@
         <div class="col-sm-3 mb-3">
           <div class="card" style="width: 15rem">
             <img src="https://m.media-amazon.com/images/M/MV5BMTgzMTEyOTgyOF5BMl5BanBnXkFtZTcwOTY1ODkxNw@@._V1_.jpg" class="card-img-top" alt="" />
-              <span class="position-absolute top 0 badge" style="baground-color: red"><?= $row -> rating;?></span>
+              <span class="position-absolute top 0 badge" style="baground-color: red"><?= $row->rating;?></span>
               <div class="card-body">
-                <a href="detail.php?id=<?= $row -> film_id; ?>">
-                  <h5 class="card-title"><?= $row -> title; ?> (<?= $row -> release_year; ?>)</h5>
+                <a href="detail.php?id=<?= $row->film_id; ?>">
+                  <h5 class="card-title"><?= $row->title; ?> (<?= $row->release_year; ?>)</h5>
                 </a>
                 <div class="col-6">
-                  <p class="card-text">Rating: <?= $row -> rating; ?></p>
+                  <p class="card-text">Rating: <?= $row->rating; ?></p>
                 </div>
                 <button class="btn" style="float: right; font-size: small"><i class="bi bi-pencil-fill" style="color:blue;"></i>
                 <i class="bi bi-trash3-fill" style="color:red"></i></button>
